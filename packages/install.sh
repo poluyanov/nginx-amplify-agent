b0ec81bc70937535e41475196f47a2f7754f08f4
Andy A <andrew@nginx.com>
2017-06-27 18:07:50 +0300

    Importing all the latest changes. Improved diagnostics. Reworked the system time check. Added an option to bypass exclude nginx on Centmin Mod.

--- packages/install.sh
+++ packages/install.sh
@@ -16,6 +16,7 @@ api_ping_url="${api_url}/ping/"
 api_receiver_url="${api_url}/1.3"
 public_ntp="north-america.pool.ntp.org"
 nginx_conf_file="/etc/nginx/nginx.conf"
+amplify_pid_file="/var/run/amplify-agent/amplify-agent.pid"
 
 #
 # Functions
@@ -116,10 +117,12 @@ get_os_name () {
 # Check what downloader is available
 check_downloader() {
     if command -V curl > /dev/null 2>&1; then
-	downloader="curl -fs"
+	downloader="curl"
+	downloader_opts="-fs"
     else
 	if command -V wget > /dev/null 2>&1; then
-	    downloader="wget -q -O -"
+	    downloader="wget"
+	    downloader_opts="-q -O -"
 	else
 	    printf "\033[31m no curl or wget found, exiting.\033[0m\n\n"
 	    exit 1
@@ -132,7 +135,7 @@ add_public_key_deb() {
     printf "\033[32m ${step}. Adding public key ...\033[0m"
 
     check_downloader && \
-    ${downloader} ${public_key_url} | \
+    ${downloader} ${downloader_opts} ${public_key_url} | \
     ${sudo_cmd} apt-key add - > /dev/null 2>&1
     
     if [ $? -ne 0 ]; then
@@ -155,7 +158,7 @@ add_public_key_rpm() {
     
     check_downloader && \
     ${sudo_cmd} rm -f /tmp/nginx_signing.key.$$ && \
-    ${downloader} ${public_key_url} | \
+    ${downloader} ${downloader_opts} ${public_key_url} | \
     tee /tmp/nginx_signing.key.$$ > /dev/null 2>&1 && \
     ${sudo_cmd} ${rpm_key_cmd} --import /tmp/nginx_signing.key.$$ && \
     rm -f /tmp/nginx_signing.key.$$
@@ -219,24 +222,24 @@ install_deb_or_rpm() {
 	printf "\033[32m\n ${step}. Updating repository ... done.\033[0m\n"
     else
 	printf "\033[31m\n ${step}. Updating repository ... failed.\033[0m\n\n"
-	printf "\033[32m Please check the list of supported systems here\033[0m https://git.io/vKkev\n\n"
+	printf "\033[32m Please check the list of the supported systems here https://git.io/vKkev\033[0m\n\n"
 	exit 1
     fi
 
     incr_step
 
     # Install package(s)
-    printf "\033[32m ${step}. Installing package ...\033[0m\n\n"
+    printf "\033[32m ${step}. Installing nginx-amplify-agent package ...\033[0m\n\n"
 
     test -n "$package_name" && \
     test -n "$install_cmd" && \
     ${sudo_cmd} ${install_cmd} ${package_name}
 
     if [ $? -eq 0 ]; then
-	printf "\033[32m\n ${step}. Installing package ... done.\033[0m\n"
+	printf "\033[32m\n ${step}. Installing nginx-amplify-agent package ... done.\033[0m\n"
     else
-	printf "\033[31m\n ${step}. Installing package ... failed.\033[0m\n\n"
-	printf "\033[32m Please check the list of supported systems here\033[0m https://git.io/vKkev\n\n"
+	printf "\033[31m\n ${step}. Installing nginx-amplify-agent package ... failed.\033[0m\n\n"
+	printf "\033[32m Please check the list of the supported systems here https://git.io/vKkev\033[0m\n\n"
 	exit 1
     fi
 }
@@ -271,6 +274,8 @@ detect_amplify_user() {
 	amplify_user=${nginx_user} || \
 	amplify_user="nginx"
     fi
+
+    amplify_group=`id -gn ${amplify_user}`
 }
 
 incr_step() {
@@ -299,7 +304,7 @@ done
 
 step=" 1"
 
-printf "\n --- This script will install the NGINX Amplify Agent ---\n\n"
+printf "\n --- This script will install the NGINX Amplify Agent package ---\n\n"
 printf "\033[32m ${step}. Checking admin user ...\033[0m"
 
 sudo_found="no"
@@ -319,7 +324,7 @@ else
     if [ "$sudo_found" = "yes" ]; then
 	printf "\033[33m you'll need sudo rights.\033[0m\n"
     else
-	printf "\033[33m not root, sudo not found, exiting.\033[0m\n"
+	printf "\033[31m not root, sudo not found, exiting.\033[0m\n"
 	exit 1
     fi
 fi
@@ -342,19 +347,40 @@ fi
 
 incr_step
 
+# Get OS name and codename
+get_os_name
+
 # Check for Python
-printf "\033[32m ${step}. Checking python version ...\033[0m"
+printf "\033[32m ${step}. Checking Python ...\033[0m"
 command -V python > /dev/null 2>&1 && python_exists='yes' || python_exists='no'
 command -V python2.7 > /dev/null 2>&1 && python_27='yes' || python_27='no'
 command -V python2.6 > /dev/null 2>&1 && python_26='yes' || python_26='no'
 
-if [ "$python_exists" = "no" ]; then
-    printf "\033[31m python 2.6 or 2.7 is required, but could not be found.\033[0m\n\n"
-    exit 1
+if [ "${python_exists}" = "yes" ]; then
+    if [ "${python_27}" = "no" -a "${python_26}" = "no" ]; then
+	printf "\033[31m version 2.6 or 2.7 is required, older or newer versions aren't supported.\033[0m\n\n"
+	pyton_exists="no"
+    fi
+else
+    printf "\033[31m version 2.6 or 2.7 is required, but could not be found.\033[0m\n\n"
 fi
 
-if [ "$python_27" = "no" -a "$python_26" = "no" ]; then
-    printf "\033[31m python 2.6 or 2.7 is required, older or newer versions aren't supported.\033[0m\n\n"
+if [ "${python_exists}" = "no" ]; then
+    case "$os" in
+	ubuntu|debian)
+	    printf "\033[32m Please check and install Python package:\033[0m\n\n"
+	    printf "     ${sudo_cmd}apt-cache pkgnames | grep python2.7\n"
+	    printf "     ${sudo_cmd}apt-get install python2.7\n\n"
+	    ;;
+	centos|amzn)
+	    printf "\033[32m Please check and install Python package:\033[0m\n\n"
+	    printf "     ${sudo_cmd}yum list python\n"
+	    printf "     ${sudo_cmd}yum install python\n\n"
+	    ;;
+	*)
+	    ;;
+    esac
+
     exit 1
 fi
 
@@ -365,9 +391,6 @@ incr_step
 
 # Check for supported OS
 printf "\033[32m ${step}. Checking OS compatibility ...\033[0m"
-
-# Get OS name and codename
-get_os_name
 
 # Add public key, create repo config, install package
 case "$os" in
@@ -409,7 +432,24 @@ case "$os" in
 
 	# Install package
 	update_cmd="yum ${assume_yes} makecache"
-	install_cmd="yum ${assume_yes} install"
+
+	# Check if nginx packages are excluded
+	if grep 'exclude.*nginx' /etc/yum.conf | grep -v '#' >/dev/null 2>&1; then
+	    printf "\n"
+	    printf "\033[32m Packages with the 'nginx' names are excluded in /etc/yum.conf - proceed with install (y/n)? \033[0m"
+
+	    read answer
+	    printf "\n"
+
+	    if [ "${answer}" = "y" -o "${answer}" = "Y" ]; then
+		install_cmd="yum ${assume_yes} --disableexcludes=main install"
+	    else
+		printf "\033[31m exiting.\033[0m\n"
+		exit 1
+	    fi
+	else
+	    install_cmd="yum ${assume_yes} install"
+	fi
 
 	install_deb_or_rpm
 	;;
@@ -451,8 +491,8 @@ fi
 
 ${sudo_cmd} rm -f ${agent_conf_file} && \
 ${sudo_cmd} sh -c "sed -e 's|api_key.*$|api_key = $api_key|' \
-                       -e 's|api_url.*$|api_url = $api_receiver_url|' \
-                       -e 's|hostname.*$|hostname = $amplify_hostname|' \
+		       -e 's|api_url.*$|api_url = $api_receiver_url|' \
+		       -e 's|hostname.*$|hostname = $amplify_hostname|' \
 	${agent_conf_file}.default > \
 	${agent_conf_file}" && \
 ${sudo_cmd} chmod 644 ${agent_conf_file} && \
@@ -475,7 +515,7 @@ amplify_euid=`id -u ${amplify_user}`
 
 if [ $? -eq 0 ]; then
     # Check is sudo can be used for tests
-    printf "\033[32m ${step}. Checking if sudo -u ${amplify_user} can be used for tests ...\033[0m"
+    printf "\033[32m ${step}. Checking if sudo -u ${amplify_user} -g ${amplify_group} can be used for tests ...\033[0m"
 
     if [ "${sudo_found}" = "yes" ]; then
 	sudo_output=`sudo -u ${amplify_user} /bin/sh -c "id -un" 2>/dev/null`
@@ -487,14 +527,14 @@ if [ $? -eq 0 ]; then
 	    errors=`expr $errors + 1`
 	fi
     else
-	printf "\033[31m failed - sudo not found.\033[0m\n"
+	printf "\033[31m failed, sudo not found.\033[0m\n"
 	errors=`expr $errors + 1`
     fi
 
     incr_step
 else
     printf "\n"
-    printf "\033[31m Something went wrong when detecting agent's user id, skipping some tests.\033[0m\n\n"
+    printf "\033[31m Can't detect the agent's user id, skipping some tests.\033[0m\n\n"
     errors=`expr $errors + 1`
 fi
 
@@ -503,25 +543,25 @@ if [ "${errors}" -eq 0 ]; then
     # Check if the agent is able to use ps(1)
     printf "\033[32m ${step}. Checking if euid ${amplify_euid}(${amplify_user}) can find root processes ...\033[0m"
 
-    sudo -u ${amplify_user} /bin/sh -c "ps xao user,pid,ppid,command" 2>&1 | grep "^root" >/dev/null 2>&1
+    sudo -u ${amplify_user} -g ${amplify_group} /bin/sh -c "ps xao user,pid,ppid,command" 2>&1 | grep "^root" >/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
 	printf "\033[32m ok.\033[0m\n"
     else
-	printf "\033[31m agent won't be able to detect nginx - ps(1) is restricted!\033[0m\n"
+	printf "\033[31m agent will fail to detect nginx, ps(1) is restricted!\033[0m\n"
 	errors=`expr $errors + 1`
     fi
 
     incr_step
 
-    printf "\033[32m ${step}. Checking if euid ${amplify_euid}(${amplify_user}) can access I/O counters ...\033[0m"
+    printf "\033[32m ${step}. Checking if euid ${amplify_euid}(${amplify_user}) can access I/O counters for nginx ...\033[0m"
 
-    sudo -u ${amplify_user} /bin/sh -c 'cat /proc/$$/io' >/dev/null 2>&1
+    sudo -u ${amplify_user} -g ${amplify_group} /bin/sh -c 'cat /proc/$$/io' >/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
 	printf "\033[32m ok.\033[0m\n"
     else
-	printf "\033[31m agent won't be able to collect I/O counters for nginx - /proc/<pid>/io is restricted!\033[0m\n"
+	printf "\033[31m failed, /proc/<pid>/io is restricted!\033[0m\n"
 	errors=`expr $errors + 1`
     fi
 
@@ -538,7 +578,7 @@ if [ -f "${nginx_conf_file}" ]; then
 	if grep -R "stub_status" ${nginx_conf_dir}/* > /dev/null 2>&1; then
 	    printf "\033[32m ok.\033[0m\n"
 	else
-	    printf "\033[31m no stub_status in nginx config, please check\033[0m https://git.io/vKTpJ\n"
+	    printf "\033[31m no stub_status in nginx config, please check https://git.io/vQGs4\033[0m\n"
 	    errors=`expr $errors + 1`
 	fi
 
@@ -546,40 +586,46 @@ if [ -f "${nginx_conf_file}" ]; then
     fi
 fi
 
-# Test connectivity to receiver
+# Test connectivity to receiver, and system time too
 if [ -n "${downloader}" ]; then
     printf "\033[32m ${step}. Checking connectivity to the receiver ...\033[0m"
 
-    if ${downloader} ${api_ping_url} | grep 'pong' >/dev/null 2>&1; then
+    if ${downloader} ${downloader_opts} ${api_ping_url} | grep 'pong' >/dev/null 2>&1; then
 	printf "\033[32m ok.\033[0m\n"
     else
-	printf "\033[31m failed to connect to the receiver! (check\033[0m https://git.io/vKk0I)\n"
+	printf "\033[31m failed to connect to the receiver! (check https://git.io/vKk0I)\033[0m\n"
 	errors=`expr $errors + 1`
     fi
 
     incr_step
-fi
 
-# Check system time
-printf "\033[32m ${step}. Checking system time with ntpdate(8) ...\033[0m"
-if command -V ntpdate > /dev/null 2>&1; then
-    ntp_offset=`ntpdate -4qu ${public_ntp} 2>&1 < /dev/null | grep -i 'ntpdate.*offset' | \
-		sed 's/.*offset [-]*\([.0-9][.0-9]*\) .*/\1/'`
-
-    test -z "${ntp_offset}" && \
-    ntp_offset=0
-
-    check_offset=`python -c "print (int)(${ntp_offset} < 5)" 2>&1`
-
-    if [ "${check_offset}" -eq 1 ]; then
-	printf "\033[32m ok.\033[0m\n"
+    # Compare server time with local time
+    printf "\033[32m ${step}. Checking system time ...\033[0m"
+    if [ "${downloader}" = "curl" ]; then
+	downloader_opts="-fsi"
     else
-	printf "\033[31m offset > 5s - please adjust the system clock for proper metric collection!\033[0m\n"
+	if [ "${downloader}" = "wget" ]; then
+	    downloader_opts="-qS"
+	fi
+    fi
+
+    server_date=`${downloader} ${downloader_opts} ${api_ping_url} 2>&1 | grep '^.*Date' | sed 's/.*Date:[ ][ ]*\(.*\)/\1/'`
+
+    if [ $? -eq 0 ]; then
+	amplify_epoch=`date --date="${server_date}" "+%s"`
+	agent_epoch=`date -u '+%s'`
+	offset=`expr ${amplify_epoch} - ${agent_epoch} | sed 's/^-//'`
+
+	if [ "${offset}" -le 6 ]; then
+	    printf "\033[32m ok.\033[0m\n"
+	else
+	    printf "\033[31m please adjust the system clock for proper metric collection!\033[0m\n"
+	    errors=`expr $errors + 1`
+	fi
+    else
+	printf "\033[31m failed!\033[0m\n"
 	errors=`expr $errors + 1`
     fi
-else
-    printf "\033[31m failed - no ntpdate installed!\033[0m\n"
-    errors=`expr $errors + 1`
 fi
 
 incr_step
@@ -599,22 +645,20 @@ printf "     \033[7m${sudo_cmd}service amplify-agent { start | stop }\033[0m\n\n
 printf "\033[32m Amplify Agent log can be found here:\033[0m\n"
 printf "     /var/log/amplify-agent/agent.log\n\n"
 
-printf "\033[32m After the agent is launched, it might take up to 1 minute this system to appear\033[0m\n"
+printf "\033[32m After the agent is launched, it takes a couple of minutes for this system to appear\033[0m\n"
 printf "\033[32m in the Amplify user interface.\033[0m\n\n"
 
 printf "\033[32m PLEASE CHECK THE DOCUMENTATION HERE:\033[0m\n"
 printf "     https://github.com/nginxinc/nginx-amplify-doc\n\n"
 
 # Check for an older version of the agent running
-if command -V pgrep > /dev/null 2>&1; then
-    agent_pid=`pgrep amplify-agent`
-else
-    agent_pid=`ps aux | grep -i '[a]mplify-agent' | awk '{print $2}'`
-fi
+if [ -f "${amplify_pid_file}" ]; then
+    amplify_pid=`cat ${amplify_pid_file}`
 
-if [ -n "$agent_pid" ]; then
-    printf "\033[32m Stopping old amplify-agent, pid ${agent_pid}\033[0m\n"
-    ${sudo_cmd} service amplify-agent stop > /dev/null 2>&1 < /dev/null
+    if ps "${amplify_pid}" >/dev/null 2>&1; then
+	printf "\033[32m Stopping old amplify-agent, pid ${amplify_pid}\033[0m\n"
+	${sudo_cmd} service amplify-agent stop > /dev/null 2>&1 < /dev/null
+    fi
 fi
 
 # Launch agent
